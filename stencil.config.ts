9d035aea00e40a736c77e61ebf1772fdbc1c9913
AmadoJunior <amadoelrepalol@gmail.com>
2024-05-03 15:18:47 -0400

    Added axios, tailwind and material-web

--- stencil.config.ts
+++ stencil.config.ts
@@ -1,8 +1,12 @@
 import { Config } from '@stencil/core';
-
+import tailwind, { tailwindHMR } from 'stencil-tailwind-plugin';
 // https://stenciljs.com/docs/config
 
 export const config: Config = {
+  plugins: [
+    tailwind(),
+    tailwindHMR(),
+  ],
   globalStyle: 'src/global/app.css',
   globalScript: 'src/global/app.ts',
   taskQueue: 'async',
