d371d68476a33b3e3825832f5e9262d9ed292ee4
AmadoJunior <amadoelrepalol@gmail.com>
2024-05-03 16:27:36 -0400

    Added prettier

--- stencil.config.ts
+++ stencil.config.ts
@@ -1,21 +1,18 @@
-import { Config } from '@stencil/core';
-import tailwind, { tailwindHMR } from 'stencil-tailwind-plugin';
+import { Config } from "@stencil/core";
+import tailwind, { tailwindHMR } from "stencil-tailwind-plugin";
 // https://stenciljs.com/docs/config
 
 export const config: Config = {
-  plugins: [
-    tailwind(),
-    tailwindHMR(),
-  ],
-  globalStyle: 'src/global/app.css',
-  globalScript: 'src/global/app.ts',
-  taskQueue: 'async',
+  plugins: [tailwind(), tailwindHMR()],
+  globalStyle: "src/global/app.css",
+  globalScript: "src/global/app.ts",
+  taskQueue: "async",
   outputTargets: [
     {
-      type: 'www',
+      type: "www",
       // comment the following line to disable service workers in production
       serviceWorker: null,
-      baseUrl: 'https://myapp.local/',
+      baseUrl: "https://myapp.local/",
     },
   ],
 };
